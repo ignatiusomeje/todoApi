f70ed8644ecb7992ef0ffd2b931ce8904c88f5e3
root <mrexcel153@gmail.com>
2018-09-24 17:15:36 +0100

    edits the packge.json start script

--- app.js
+++ app.js
@@ -60,7 +60,5 @@ a127.init(function(config) {
   // begin listening for client requests
   app.listen(port, ip);
 
-  console.log(ip);
-
   console.log('try this:\ncurl http://' + ip + ':' + port + '/hello?name=Scott');
 });
